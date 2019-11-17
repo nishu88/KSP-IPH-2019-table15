1537209252aa84653be889ef6aba04049b48ee1e
Mahadev Maitri <31066598+mahadev9@users.noreply.github.com>
2019-11-17 13:06:40 +0530

    Android Client For Face Recognition

---
+++ Android App with Flask Backend/AndroidClient/app/build/generated/source/buildConfig/debug/gad/heartbeat/androidflask/easyupload/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package gad.heartbeat.androidflask.easyupload;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "gad.heartbeat.androidflask.easyupload";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
